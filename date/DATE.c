f1e2f62afdd9fea93576db9655d60513e6f78164
Manar Mighri <144473914+mighri-manar@users.noreply.github.com>
2025-10-23 23:26:20 +0100

    Add files via upload

---
+++ date/DATE.c
@@ -0,0 +1,49 @@
+//
+// Created by manar mighri on 14/11/2023.
+//
+
+#include "DATE.h"
+#include<stdio.h>
+
+void Input_date(DATE* D)
+{
+    printf("\nDay\n");
+    fflush(stdin);
+    scanf("%d",&D->day);
+    printf("\nMonth\n");
+    scanf("%d",&D->month);
+    printf("\nYear\n");
+    scanf("%d",&D->year);
+}
+/*----------------------------------------*/
+void Show_date(DATE D)
+{
+    printf("\n%d/",D.day);
+    printf("%d/",D.month);
+    printf("%d\n",D.year);
+}
+/*----------------------------------------*/
+ char *Return_date(DATE D)
+{
+
+    static char date_string[11];
+    sprintf(date_string, "%02d/%02d/%04d", D.day, D.month, D.year);
+    return date_string;
+}
+/*----------------------------------------*/
+DATE Read_date( char* D)
+{
+    DATE date;
+    sscanf(D, "%d/%d/%d", &date.day, &date.month, &date.year);
+    return date;
+}
+/*----------------------------------------*/
+void initialize_date(DATE* D)
+{
+    D->day=0;
+    D->month=0;
+    D->year=0;
+}
+
+
+
---
+++ date/DATE.h
@@ -0,0 +1,20 @@
+//
+// Created by manar mighri on 14/11/2023.
+//
+
+#ifndef STRUCTUREMESSAGE_DATE_H
+#define STRUCTUREMESSAGE_DATE_H
+
+typedef struct
+{
+    int day, month, year;
+}DATE;
+void Input_date(DATE*);
+void Show_date(DATE);
+
+//these functions were replaced with time.h library
+char* Return_date(DATE);
+DATE Read_date(char*);
+void Initialize_date(DATE*);
+
+#endif //STRUCTUREMESSAGE_DATE_H
