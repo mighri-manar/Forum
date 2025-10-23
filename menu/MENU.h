83010b73dac648f34fddde2e2e9a492ed8c07010
Manar Mighri <144473914+mighri-manar@users.noreply.github.com>
2025-10-23 23:31:00 +0100

    Add files via upload

---
+++ menu/MENU.h
@@ -0,0 +1,36 @@
+//
+// Created by manar mighri on 21/11/2023.
+//
+
+#ifndef STRUCTUREMESSAGE_MENU_H
+#define STRUCTUREMESSAGE_MENU_H
+
+
+typedef struct
+{}MENU;
+
+
+int Welcome();
+int Menu();
+int Menu_visit();
+int Menu_visit_explore();
+int Menu_visit_rooms_select_room();
+int Menu_visit_rooms_select_room_select_quest();
+int Menu_login_explore();
+int Menu_login_explore_add_new_room();
+int Menu_login_explore_rooms();
+int Menu_login_explore_rooms_select_room();
+int Menu_login_explore_rooms_select_room_select_quest();
+int Menu_login_about();
+int Menu_login_settings();
+int Menu_login_settings_account();
+int Menu_delete_account();
+int Menu_login_settings_posts();
+int Menu_login_settings_posts_questions();
+int Menu_login_settings_posts_questions_select();
+int Menu_login_about_users();
+
+
+
+
+#endif //STRUCTUREMESSAGE_MENU_H
